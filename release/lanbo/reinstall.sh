2357adc61fb49e98da8b4e3fe7d6155e3389abbe
tarvfx <tarvfx@pm.me>
2023-05-12 17:29:41 +0800

    feat: add lanbo shell script

---
+++ release/lanbo/reinstall.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+if [ -d /usr/local/go ]; then
+  export PATH="$PATH:/usr/local/go/bin"
+fi
+
+DIR=$(dirname "$0")
+PROJECT=$DIR/../..
+
+pushd $PROJECT
+#go install -v -trimpath -ldflags "-s -w -buildid=" -tags with_quic,with_wireguard,with_acme ./cmd/sing-box
+go install -v -trimpath -ldflags "-s -w -buildid=" ./cmd/sing-box
+popd
+
+sudo systemctl stop lanbo
+sudo cp $(go env GOPATH)/bin/lanbo /usr/local/bin/
+sudo systemctl start lanbo
